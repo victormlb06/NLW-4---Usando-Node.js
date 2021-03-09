60f8a2ec9878f0ba147b52d89f1a2b9f8353697f
Victor Brandao <brandaovictor06@gmail.com>
2021-03-09 14:24:27 -0300

    Aula 2 -  Banco de Dados

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
+import { router } from "./Router";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.send("Hello World");
-});
-
-app.post("/a", (request, response) => {
-  return response.json("Os dados foram salvos com sucesso");
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3200, () => console.log("The server is running"));
